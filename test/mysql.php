e70ce15963251e8d3574d7aec59d28bb87c6f630
jxinchao <425775961@qq.com>
2018-07-06 09:56:29 +0800

    ..

---
+++ test/mysql.php
@@ -0,0 +1,13 @@
+<?php
+
+  /*
+  使用 cmd 链接数据库
+
+  windows + R  调用cmd
+
+  输入命令 mysql -uroot -p   然后输入密码
+
+  如果提示MySQL不是内部命令    需要配置环境变量    地址为mysql安装的位置 F:\wamp\bin\mysql\mysql5.7.9\bin
+  */
+
+?>
